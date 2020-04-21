0d26f946d8ee62187bec001ef0e0572389b2bf97
Mert  Ozkaynak <mertozkaynak@windowslive.com>
2020-04-21 17:29:52 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,15 +1,15 @@
 def count_strings(array)
   
-  array.count do |string| 
-    string.is_a? (String)
-end 
+  array.count { |somn| 
+    somn.is_a? (String)
+}
 end
 
 # Return the total number of strings in the provided array using the count enumerable
 
 def count_empty_strings(array)
- array.count do |string|
-   string == ""
+ array.count do |natn|
+   natn == ""
  end 
 end
 
